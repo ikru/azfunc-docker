a600f825e2c21c53176e190c8ab04f5a58fff8f3
ikru <ikru@users.noreply.github.com>
2021-05-06 08:56:15 -0400

    Update func-deploy.sh

--- scripts/func-deploy.sh
+++ scripts/func-deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker-compose exec "azfunc" bash -c "dotnet publish azfunc/*csproj -o ../out && cd ../out && zip app.zip -r * && curl -w 'curl: %{http_code}\n' --insecure -X POST -u \"ftpuser:password\" --data-binary \"@./app.zip\""
+docker-compose exec "azfunc" bash -c "dotnet publish azfunc/*csproj -o ../out && cd ../out && zip app.zip -r * && curl -w 'curl: %{http_code}\n' --insecure -X POST -u \"ftpuser:password\" --data-binary \"@./app.zip\" https://scmurl/api/zipdeploy"
